57017a956c9eb256e4d84a31d2aacdfa6c116da6
zhbhun <zhbhun@gmail.com>
2024-04-23 07:48:53 +0800

    docs: add model of modenet

---
+++ packages/model-modnet/index.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'modnet',
+  files: ['modnet-1.onnx', 'modnet-2.onnx', 'modnet-3.onnx'],
+  mime: 'application/octet-stream',
+  publicPath: 'https://unpkg.com/@rmbg/model-modnet@0.0.1/',
+  resolution: 512,
+  size: 25888640
+}
